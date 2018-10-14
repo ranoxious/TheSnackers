f0240f30c232e25af6e3d4cf4d7c467c29b97225
ranoxious <28329189+ranoxious@users.noreply.github.com>
2018-10-14 15:48:15 -0400

    Add files via upload

--- TheSnackers/TheSnackers/server.js
+++ TheSnackers/TheSnackers/server.js
@@ -1,32 +1,27 @@
 const express = require('express');
 const cors = require('cors');
+const path = require("path");
+const bodyParser = require("body-parser");
 const app = express();
 const mongoose = require('mongoose');
 const routes = require('./routes');
 const db = require('./models');
 
 var port = process.env.PORT || 3029;
-
-
 var router = express.Router();
 
-const routes = {
-  products: {
-    get: 'api/products'
-  }
-};
-
+// Define middleware here
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+// Serve up static assets (usually on heroku)
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static("client/build"));
+}
 
 app.use(cors());
 
+app.use(routes);
 
-app.get('*', function(req, res){
-	res.sendFile(path.resolve(__dirname, 'public', 'index.html'));
-  });
-
-app.use('*', function (req, res) {
-    res.redirect(routes.products.get);
-});
 
 
 app.listen(port, () => {
